0beea6125bb4c67b611d1a6825809999803b882c
Cristian Berlo <cristianeberlo@gmail.com>
2025-05-20 01:10:54 -0300

    modelos agregados

--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    id= models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=70)
+    description = models.CharField(max_length=1250)
+    publicationDate =  models.DateTimeField()
+    genres= models.CharField(max_length=50)
+
+
+class Credential(models.Model):
+    User = models.CharField(max_length=35)
+    passwd =  models.CharField(max_length=35)
+    email = models.CharField(max_length=35)
+
+class User(Credential):
+    nick = models.CharField(max_length=35)
+    joinDate = models.DateTimeField()
+
+ 
+class Edition(models.Model):
+    id= models.IntegerField(primary_key=True)
+    title= models.CharField(max_length=70)
+    publicationDate=  models.DateTimeField()
+    publisher= models.CharField(max_length=70)
+    lang= models.CharField(max_length=10)
+    pages= models.IntegerField()
+    notes= models.CharField(max_length=500)
+    place= models.CharField(max_length=70)
+    cover= models.CharField(max_length=100)
+    file= models.CharField(max_length=100)
+
+class Autor(models.Model):
+    name= models.CharField(max_length=35)
+    lastName= models.CharField(max_length=35)
+    bio= models.CharField(max_length=1250)
+    birth= models.DateTimeField()
+    death= models.DateTimeField()
+    prof= models.CharField(max_length=35)
+    portrait= models.CharField(max_length=100)
--- home/settings.py
+++ home/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'home.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'freelibrary',        
+        'USER': 'freelibraryadmin',            
+        'PASSWORD': 'admin',     
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
