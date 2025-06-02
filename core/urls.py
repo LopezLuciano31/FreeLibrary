861084844c13ba2bd9e1dc9c8eb2179b878f3b4d
Cristian Berlo <cristianeberlo@gmail.com>
2025-06-02 14:31:19 -0300

    cambio modelos, agregados imgfield y filefield

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 from .views import loginUserV
 from .views import registerUserV
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.homepage, name='homepage'),
@@ -10,7 +11,12 @@ urlpatterns = [
     path('reader/', views.reader),
     path('login/', loginUserV, name='login'),
     path('register/', registerUserV, name='register'),
+    path('autorRegister/', views.autorRegister, name='autorRegister'),
+    path('bookRegister/', views.bookRegister, name='bookRegister'),
+    path('editionRegister/', views.editionRegister, name='editionRegister'),
     path('profile/', views.profileUser, name='profile'),
     path('mybooks/', views.mybooks),
     path('book_list/',views.book_list, name='book_list')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
