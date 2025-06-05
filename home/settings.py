f66d9ead38c10b2b07aedec678192adc83c122f1
Cristian Berlo <cristianeberlo@gmail.com>
2025-06-05 02:03:36 -0300

    archivos faltantes commit anterior

--- home/settings.py
+++ home/settings.py
@@ -128,5 +128,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER =  os.getenv("MAIL_FREELIBRARY"),  
+EMAIL_HOST_PASSWORD =  os.getenv("PWD__FREELIBRARY"),  
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
