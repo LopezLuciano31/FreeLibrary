72d8cc5e72b6b467354afc650fd2c38402c04c31
cristianBerlo <cristianeberlo@gmail.com>
2025-06-06 21:50:07 -0300

    Agregado mail personalizado, mail se muestra por terminal

--- home/settings.py
+++ home/settings.py
@@ -128,10 +128,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER =  os.getenv("MAIL_FREELIBRARY"),  
-EMAIL_HOST_PASSWORD =  os.getenv("PWD_MAIL_FREELIBRARY"),  
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_HOST_PASSWORD = os.getenv("PWD_MAIL_FREELIBRARY"),  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
