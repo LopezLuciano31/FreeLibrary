d537709067557cc28ad4a517e34729ff22c2405d
Cristian Berlo <cristianeberlo@gmail.com>
2025-06-09 00:20:47 -0300

    Formularios autor, edicion, libro traducidos

--- core/forms.py
+++ core/forms.py
@@ -6,23 +6,53 @@ class EditionForm(ModelForm):
     class Meta:
         model = Edition
         fields = ["id","title", "publicationDate","publisher","lang","pages","notes","place","cover","file","of"]
-        widgets = {
-            'publicationDate': forms.DateInput(attrs={'type': 'date'}),
+        labels = {
+            'title': 'Titulo edicion:',
+            'lang': 'Lenguaje:',
+            'publicationDate': 'Fecha de publlicacion:',
+            'pages': 'Numero de paginas:',
+            'id':'Identificador numerico:',
+            'cover':'Portada:',
+            'file':'Archivo:',
+            'publisher':'Editorial:',
+            'notes':'Notas:',
+            'place':'Lugar:',
+            'of':'Libro:'
         }
+        widgets = {
+            'publicationDate': forms.DateInput(attrs={ 'type': 'date',}),}
 
+      
 class BookForm(ModelForm):
     class Meta:
         model = Book
         fields = ["id","name", "description", "publicationDate","genres","autor"]
-        widgets = {
-            'publicationDate': forms.DateInput(attrs={'type': 'date'}),
+        labels = {
+            'name': 'Titulo:',
+            'description': 'Descripcion:',
+            'publicationDate': 'Fecha de publlicacion:',
+            'autor': 'Autor:',
+            'id':'Identificador numerico:',
+            'genres':'Generos:'
         }
+        widgets = {
+            'publicationDate': forms.DateInput(attrs={ 'type': 'date',}),}
+
 
 class AutorForm(ModelForm):
     class Meta:
         model = Autor
         fields = ["name", "lastName", "bio","birth","death","prof","portrait"]
-        widgets = {
-            'birth': forms.DateInput(attrs={'type': 'date'}),
-            'death': forms.DateInput(attrs={'type': 'date'})  
+        labels = {
+            'name': 'Nombre:',
+            'lastName': 'Apellido:',
+            'bio': 'Biografia:',
+            'prof': 'Profesion:',
+            'portrait': 'Retrato:',
+            'birth': 'Fecha de nacimiento',
+            'death': 'Fecha de defuncion',
         }
+        widgets = {
+            'birth': forms.DateInput(attrs={  'type': 'date' }),
+            'death': forms.DateInput(attrs={  'type': 'date' }),
+        } 
