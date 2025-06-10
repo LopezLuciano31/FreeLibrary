51742e5ee8acaa5b4fadbbbfec99a71182a7d7aa
Cristian Berlo <cristianeberlo@gmail.com>
2025-06-10 00:49:15 -0300

    prueba para despliegue web

--- home/settings.py
+++ home/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-iu+d^qwdyd8pu9qsgxdl&x5zb2$hpj--53vb(u59iz&lv@)*bl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','https://freelibraryunsl.onrender.com/']
 
 
 # Application definition
